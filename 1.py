5a0514d49b3998fded045eb767e13b848ededa13
Simons <121897649+SimonGamer1234@users.noreply.github.com>
2025-01-21 21:54:17 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,11 +5,25 @@ import time
 import json
 
 # Retrieve the environment variables
-repo_var_one = os.getenv("REPO_VAR_ONE")
+AD1 = os.getenv("REPO_VAR_1")
+AD2 = os.getenv("REPO_VAR_2")
+AD3 = os.getenv("REPO_VAR_3")
+AD4 = os.getenv("REPO_VAR_4")
+AD5 = os.getenv("REPO_VAR_5")
+AD6 = os.getenv("REPO_VAR_6")
+AD7 = os.getenv("REPO_VAR_7")
+AD8 = os.getenv("REPO_VAR_8")
+AD9 = os.getenv("REPO_VAR_9")
+AD10 = os.getenv("REPO_VAR_10")
+AD11 = os.getenv("REPO_VAR_11")
+AD12 = os.getenv("REPO_VAR_12")
 repo_var_two = os.getenv("REPO_VAR_TWO")
 secret_var = os.getenv("SECRET_VAR")
 
 urls = repo_var_two.split(',')
+
+
+CurrentAd = 
 
 # Print them or use them in your script
 print(f"Repo Variable One: {repo_var_one}")
@@ -17,7 +31,7 @@ print(f"Repo Variable Two: {repo_var_two}")
 print(f"Secret Variable: {secret_var}")
 
 header = {"Authorization": secret_var}
-payload = {"content": repo_var_one}
+payload = {"content": CurrentAd}
 
 # Loop through the links and make POST requests
 for link in urls:
