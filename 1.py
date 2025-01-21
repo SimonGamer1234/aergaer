e253535fad346f5f7f69f71e121cdaf3d8861be7
Simons <121897649+SimonGamer1234@users.noreply.github.com>
2025-01-21 18:08:02 +0100

    Update 1.py

--- 1.py
+++ 1.py
@@ -9,3 +9,13 @@ secret_var = os.getenv("SECRET_VAR")
 print(f"Repo Variable One: {repo_var_one}")
 print(f"Repo Variable Two: {repo_var_two}")
 print(f"Secret Variable: {secret_var}")
+
+header = {"Authorization": secret_var}
+payload = {"content": repo_var_one}
+
+for link in repo_var_two:
+    sleeptime = random.uniform(2,3)
+    res = requests.post(link, data=payload, headers=header)
+    print(f"Posted to {link}: {res.status_code}")  # Print response status
+    print(f"waiting {sleeptime}")
+    time.sleep(sleeptime)  # Wait 5 seconds before the next request
