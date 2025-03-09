63050820437a76d512bb735e444036b855b0ad18
oskarix7 <oskar.vyborny@gmail.com>
2025-03-09 11:44:55 +0100

    Fix ad variable delimiter in UpdateVariable function

--- script.py
+++ script.py
@@ -69,15 +69,15 @@ def SearchForPosts(Keyword, ids, author_ids):
   return totalcount
 
 def UpdateVariable(Ad):
-  AdContent = Ad.split("\n=divider\n")[0]
-  TotalPosts = Ad.split("\n=divider\n")[1]
-  DaysLeft = Ad.split("\n=divider\n")[2]
-  KeyWords = Ad.split("\n=divider\n")[3]
+  AdContent = Ad.split("\n=divider=\n")[0]
+  TotalPosts = Ad.split("\n=divider=\n")[1]
+  DaysLeft = Ad.split("\n=divider=\n")[2]
+  KeyWords = Ad.split("\n=divider=\n")[3]
   NewDays = int(DaysLeft) - 1
   if NewDays == 0:
      SendMessage(f"Ad {AdContent} has expired", BOT_TOKEN, "https://discord.com/api/v9/channels/1302654558023057540/messages")
   NAME = f"AD_{Ads.index(Ad) + 1}"
-  Text = f"{AdContent}\n=divider\n{TotalPosts}\n=divider\n{NewDays}\n=divider\n{KeyWords}"
+  Text = f"{AdContent}\n=divider=\n{TotalPosts}\n=divider=\n{NewDays}\n=divider=\n{KeyWords}"
   headers = {
     'Accept': 'application/vnd.github+json',
     'Authorization': f'Bearer {GITHUB_TOKEN}',
